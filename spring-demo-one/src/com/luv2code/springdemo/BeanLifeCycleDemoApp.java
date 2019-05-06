e05c0a8a80ffafcce8578b02876543e4c7c44fce
victormsti <victormsti@gmail.com>
2019-05-06 14:44:03 -0300

    added new files

--- spring-demo-one/src/com/luv2code/springdemo/BeanLifeCycleDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/BeanLifeCycleDemoApp.java
@@ -6,27 +6,17 @@ public class BeanLifeCycleDemoApp {
 
 	public static void main(String[] args) {
 
-				// load the spring configuration file
-				ClassPathXmlApplicationContext context =
-						new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
-				
-				// retrieve bean from spring container
-				Coach theCoach = context.getBean("myCoach", Coach.class);
-				
-				Coach alphaCoach = context.getBean("myCoach", Coach.class);
-				
-				// check if they are the same bean
-				boolean result = (theCoach == alphaCoach);
-				
-				// print out the results
-				System.out.println("\nPointing to the same object: " + result);
-				
-				System.out.println("\nMemory location for theCoach: " + theCoach);
-				
-				System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
-				
-				// close the context
-				context.close();
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+		
+		// retrieve bean from spring container
+		TrackCoach theCoach = context.getBean("myCoach", TrackCoach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// close the context
+		context.close();
 	}
 
 }
--- spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -8,12 +8,22 @@ public class BeanScopeDemoApp {
 
 				// load the spring configuration file
 				ClassPathXmlApplicationContext context =
-						new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+						new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
 				
 				// retrieve bean from spring container
 				Coach theCoach = context.getBean("myCoach", Coach.class);
 				
-				System.out.println(theCoach.getDailyWorkout());
+				Coach alphaCoach = context.getBean("myCoach", Coach.class);
+				
+				// check if they are the same bean
+				boolean result = (theCoach == alphaCoach);
+				
+				// print out the results
+				System.out.println("\nPointing to the same object: " + result);
+				
+				System.out.println("\nMemory location for theCoach: " + theCoach);
+				
+				System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
 				
 				// close the context
 				context.close();
